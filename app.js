18aca36129e0ba5cfeea60aab2229f2e3654c01a
AbdullayevTemurmalik <tmaq77@gamil,com>
2026-01-06 12:23:44 +0500

    Express.JS-4-dars

--- app.js
+++ app.js
@@ -8,55 +8,22 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-  try {
-    const allPosts = await postModel.find();
-    res.status(200).json(allPosts);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
-
-app.post("/", async (req, res) => {
-  try {
-    const { title, body } = req.body;
-    const newPost = await postModel.create({ title, body });
-    res.status(201).json(newPost);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
-
-app.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  res.send(id);
-});
-
-app.put("/:id", (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-
-  res.json({ id, body });
-});
+// Routes
+app.use("/api/post", require("./routes/post.route"));
 
 const PORT = process.env.PORT || 8080;
 
 const bootstrap = async () => {
   try {
-    const dbUrl = process.env.DB_URL;
-    
-    if (!dbUrl) {
-      throw new Error("DB_URL .env faylida aniqlanmagan!");
-    }
-
-    await mongoose.connect(dbUrl);
-    console.log("Connected DB");
+    await mongoose
+      .connect(process.env.DB_URL)
+      .then(() => console.log("Connected DB"));
 
     app.listen(PORT, () =>
       console.log(`Listening on - http://localhost:${PORT}`)
     );
   } catch (error) {
-    console.log(`Error connecting with DB: ${error.message}`);
+    console.log(`Error connecting with DB: ${error}`);
   }
 };
 
